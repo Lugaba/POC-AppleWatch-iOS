4c5f96e7876be45289be5fb83448f2163a322036
Luca Hummel <luca.hummel12@gmail.com>
2022-10-24 14:34:57 -0300

    Initial Commit

---
+++ watchTeste2/watchTeste2App.swift
@@ -0,0 +1,17 @@
+//
+//  watchTeste2App.swift
+//  watchTeste2
+//
+//  Created by Luca Hummel on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct watchTeste2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
